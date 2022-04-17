6a9e188cac41f9aafa4267915fb542bc6cc7ca35
arif99050 <arifulislam99050@gmail.com>
2022-04-17 15:54:35 +0600

    work with registration form

--- src/Pages/LogIn/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn/LogIn.js
@@ -1,15 +1,65 @@
-import React from "react";
-import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, Form, Toast, ToastBody, ToastContainer } from "react-bootstrap";
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const LogIn = () => {
+    const [email,setEmail]=useState(' ')
+    const [password,setPassword]=useState(' ')
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    let errorElement;
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+     
+    const handleEmail=e=>{
+        setEmail(e.target.value)
+        console.log(e.target.value)
+    }
+    const handlePassword=e=>{
+        setPassword(e.target.value)
+        console.log(e.target.value)
+    }
+    const handleForm =e=>{
+        e.preventDefault();
+        signInWithEmailAndPassword(email,password)
+    }
+    if (loading || sending) {
+        return <h1>Loading ....</h1>
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+    const resetPassword = async () => {
+   
+        if (email) {
+            await sendPasswordResetEmail(email);
+            Toast('Sent email');
+        }
+        else{
+            Toast('please enter your email address');
+        }
+    }
+
   return (
-    <div style={{border:"1px solid black", height:"500px"}} className="bg-primary w-75 mx-auto mt-5">
-      <Form className="w-50 mx-auto bg-white p-4 mt-5" style={{"border-radius":"10px"}}>
+    <div style={{border:"1px solid black", height:"500px"}} className="bg-primary w-75 mx-auto mt-5 mb-5">
+      <Form onSubmit={handleForm} className="w-50 mx-auto bg-white p-4 mt-5" style={{borderRadius:"10px"}}>
         
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter your email" />
+          <Form.Control onBlur={handleEmail} type="email" placeholder="Enter your email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -17,16 +67,18 @@ const LogIn = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control onBlur={handlePassword} type="password" placeholder="Password" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+       
         <p>New to Workout with arif? <Link to="/registration">please register</Link> </p>
+        
         <Button variant="primary" type="submit">
           Submit
         </Button>
+        {errorElement}
+        <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
       </Form>
+      <ToastContainer />
     </div>
   );
 };
--- src/Pages/LogIn/Registration/Registration.js
+++ src/Pages/LogIn/Registration/Registration.js
@@ -1,19 +1,69 @@
-import React from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Registration = () => {
-    return (
-        <div style={{border:"1px solid black", height:"500px"}} className="bg-primary w-75 mx-auto mt-5">
-      <Form className="w-50 mx-auto bg-white p-4 mt-5" style={{"border-radius":"10px"}}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+  const [agree, setAgree] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const [createUserWithEmailAndPassword, loading, error] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+  if (loading) {
+    return <h1>loading....</h1>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    // console.log(e.target.value)
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleName = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(email, password);
+
+    navigate("/");
+  };
+
+  return (
+    <div
+      style={{ border: "1px solid black", height: "650px" }}
+      className="bg-primary w-75 mx-auto mt-5 d-flex align-items-center"
+    >
+      <Form
+        onSubmit={handleFormSubmit}
+        className="w-50 mx-auto bg-white p-4 mt-5 mb-3"
+        style={{ borderRadius: "10px" }}
+      >
+        <Form.Group className="mb-3" controlId="formBasicText">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your name" />
-          
+          <Form.Control
+            onBlur={handleName}
+            type="text"
+            placeholder="Enter your name"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter your email" />
+          <Form.Control
+            onBlur={handleEmail}
+            type="email"
+            placeholder="Enter your email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -21,18 +71,35 @@ const Registration = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            onBlur={handlePassword}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <p>Already have accound? <Link to="/login">Please Log In</Link> </p>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+
+        <p>
+          Already have accound? <Link to="/login">Please Log In</Link>
+        </p>
+        <input
+          onClick={() => setAgree(!agree)}
+          type="checkbox"
+          name="terms"
+          id="terms"
+        />
+        <label className={`ps-2 ${agree ? "" : "text-danger"}`} htmlFor="terms">
+          AcceptTerms and Conditions
+        </label> <br />
+        <input
+          disabled={!agree}
+          className=" mx-auto btn btn-primary mt-2"
+          type="submit"
+          value="Register"
+        />
       </Form>
     </div>
-    );
+  );
 };
 
 export default Registration;
